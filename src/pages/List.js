699c3385f5c25a85233551b9b3598d1ab6300e2e
Nelio Alves <jefferson2112@hotmail.com>
2019-11-14 23:48:47 -0300

    App Mobile

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-  return <View>
-    <Text>Page List</Text>
-  </View>
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    })
+  }, []);
+
+  return (
+    <ScrollView>
+      <SafeAreaView style={StyleSheet.container}>
+        <Image source={logo} />
+
+        {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+
+      </SafeAreaView>
+    </ScrollView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10
+  }
+});
